6161e341930cf717fa63166e47db342096bfba72
909banks <32732657+909banks@users.noreply.github.com>
2020-03-08 13:54:59 +0000

    Added ontology wrapper file
    This file is used to interact with the ontology via GraphDB. The program is implemented as a class with methods for connecting to the ontology, running graphDB if not already executing, querying the ontology to retireve all the child nodes of the current person/company selected, and a method for processing the results to be formatted in the correct way for use by the search algorithm.

---
+++ Software/ontology_wrapper.py
@@ -0,0 +1,89 @@
+import pycurl
+import os
+import socket, errno
+from SPARQLWrapper import SPARQLWrapper, CSV
+
+class Interface:
+    def __init__(self, url):
+        # Default values for the graphURL and default port, these values are provided
+        # by the top-level python file search.py
+        self.graphURL=url
+        self.sparql=SPARQLWrapper(url)
+        self.defaultPort=7200
+
+    def startGraphDB(self, graphExecutable):
+        """
+        Method used to run the graphDB executable if it is installed, else
+        it will raise an exception and tell the user to install graphDBFree
+        """
+
+        # Need to remove any extra quotation marks in the string necessary for the
+        # os.system execute, to be accessable for the os.path commands
+        filePath=graphExecutable.replace('"','')
+        if not os.path.exists(filePath) or not os.access(filePath, os.X_OK):
+            print(os.path.exists(filePath))
+            print(os.access(filePath, os.X_OK))
+            raise OSError ("GraphDB not found at %s, please install and re-run this program" % (graphExecutable))
+        os.system(graphExecutable)
+
+    def connectToGraph(self, graphExecutable, graphURL):
+        """
+        This method is used to connect to the ontology onstart-up if necessary, 
+        it will ensure GraphDB is running and has a stable connection for us to 
+        query the ontology with
+        """
+        graphRunning=False
+        socketOpen=False
+        s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host=socket.gethostname()
+
+        # Strat GraphDB and connect to the default port with the correct repository
+        # We have assumed that the repository is already connected and set as the 
+        # default repository
+        while socketOpen==False:
+
+            # Attempt to open the port, if GraphDB is already open it will be unable 
+            # to bind and throw an error, if the port is free we can query the ontology
+            try:
+                s.bind((host, self.defaultPort))
+                s.close()
+
+            except socket.error as e:
+                if e.errno == errno.EADDRINUSE:
+                    graphRunning=True
+                    socketOpen=True
+
+                elif graphRunning==False:
+                    print("Starting GraphDB connection")
+                    self.startGraphDB(graphExecutable)
+                    graphRunning=True
+
+                else:
+                    print("No ontology found")
+                    exit()
+        
+        self.graphURL=graphURL
+        self.sparql=SPARQLWrapper(self.graphURL)
+        return True
+
+
+    def queryOntology(self, name=str):
+        """
+        Method used to query the selected ontology using the python SPARQL interface
+        """
+
+        self.sparql.setQuery('''
+            PREFIX rdfs: <http://www.ontotext.com/explicit>
+            SELECT
+            WHERE
+        ''')
+
+        self.sparql.setReturnFormat(CSV)
+        results = self.sparql.query().convert()
+        print(results)
+
+        return results
+
+
+    def processQueryResults(self, results=[str]):
+        pass
